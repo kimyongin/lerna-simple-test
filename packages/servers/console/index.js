e175bbec7b47f9f4da77a3df76231aa2464e5221
kimyongin <kimyongin@nexon.co.kr>
2021-09-07 09:50:00 +0900

    docker 추가

--- packages/servers/console/index.js
+++ packages/servers/console/index.js
@@ -1,8 +1,23 @@
+const express = require('express')
 const greeter = require("greeter")
 const {config} = require("common");
+
 function hello() {
     console.log(process.cwd())
     console.log(config.get("name"))
-    console.log(greeter("console"))
+    let hello = greeter("console");
+    console.log(hello)
+    return hello
 }
 hello()
+
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+    res.send(hello())
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
--- packages/servers/sender/index.js
+++ packages/servers/sender/index.js
@@ -1,9 +1,23 @@
+const express = require('express')
 const greeter = require("greeter")
 const {config} = require("common");
 
 function hello() {
     console.log(process.cwd())
     console.log(config.get("name"))
-    console.log(greeter("sender"))
+    let hello = greeter("sender");
+    console.log(hello)
+    return hello
 }
 hello()
+
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+    res.send(hello())
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
